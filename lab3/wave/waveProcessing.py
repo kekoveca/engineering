bcf7299a0a749976eb5fa4a1038f50bb9f23eef8
Kulanov Alex <kulanov.av@phystech.edu>
2021-11-30 17:47:30 +0300

    lab3.1

--- lab3/wave/waveProcessing.py
+++ lab3/wave/waveProcessing.py
@@ -1,8 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-## CALIBRATION
-
 d20mm = np.loadtxt("wave-starter-kit/data example/20 mm.txt", dtype= int, skiprows=4)
 d40mm = np.loadtxt("wave-starter-kit/data example/40 mm.txt", dtype= int, skiprows=4)
 d60mm = np.loadtxt("wave-starter-kit/data example/60 mm.txt", dtype= int, skiprows=4)
@@ -26,101 +24,32 @@ yfit = np.polyval(plf, data)
 leg = str(" N = {:.3f}P + {:.3f}".format(plf[1], plf[0]))
 
 fig1, ax = plt.subplots(figsize = (16, 10), dpi = 400)
-ax.plot(data, yfit,  label = leg, lw = 2, c = 'red')
-plt.ylabel("ADC counts")
-plt.xlabel("Pressure, mmHg")
-plt.title("Calibration graph N(P)", fontsize = 20)
+ax.plot(data, yfit,  label = leg, lw = 2, c = 'orange')
+ax.scatter(data, meanarr)
+#plt.ylabel("ADC counts")
+#plt.xlabel("Pressure, mmHg")
+#plt.title("Calibration graph N(P)", fontsize = 20)
 ax.set_ylim(2000, 3000)
-ax.legend()
+#ax.legend()
 ax.grid(b=True, which='major', color='grey', linestyle='-')
 ax.grid(b=True, which='minor', color='grey', linestyle='--')
 plt.minorticks_on()
 fig1.savefig("1.png")
 
-'''
-## FITNESS 
+wave = np.loadtxt("wave-starter-kit/data example/wave.txt", dtype= int, skiprows=4)
+dataw = np.linspace(0, 30, len(wave))
 
-dataRawFitness = np.loadtxt("blood/data/fitness.txt", dtype= int)
-dataFit = dataRawFitness/k - y0/k
-time = np.linspace(0, 60, len(dataFit))
-tstep = len(time)/60
-leg1 = str("Pressure = {}/{}".format(147,97))
+plf1 = np.polyfit(dataw, wave, 2)
+yfit1 = np.polyval(plf1, dataw)
 
 fig2, ax = plt.subplots(figsize = (16, 10), dpi = 400)
-ax.plot(time, dataFit, lw = 1, c = 'brown', marker = 'o', markevery = [6*tstep, 41.7*tstep], label = leg1,
-markersize = 15, markerfacecolor = 'red', markeredgecolor = 'red')
-ax.legend()
-plt.ylabel("Pressure, mmHg")
-plt.xlabel("Time, seconds")
-plt.title("Blood pressure after fitness", fontsize = 25)
-ax.text(0.12, 0.75, 'Systole', fontsize = 20, transform=ax.transAxes)
-ax.text(0.82, 0.2, 'Diastole', fontsize = 20, transform=ax.transAxes)
+ax.plot(dataw, wave,lw = 2, c = 'orange')
+#plt.ylabel("ADC counts")
+#plt.xlabel("Pressure, mmHg")
+#plt.title("Calibration graph N(P)", fontsize = 20)
+ax.set_xlim(0, 1.5*dataw)
+#ax.legend()
 ax.grid(b=True, which='major', color='grey', linestyle='-')
 ax.grid(b=True, which='minor', color='grey', linestyle='--')
 plt.minorticks_on()
-
-fig2.savefig("fintess-pressure.png")
-
-##FITNESS PULSE
-
-pplf = np.polyfit(time, dataFit, 2)
-pyfit = np.polyval(pplf, time)
-deltadata1 = pyfit - dataFit
-legp1 = str("The pulse is {:.0f} ".format(60*(16)/(28-18)))
-
-figp2, ax = plt.subplots(figsize = (16, 10), dpi = 400)
-ax.plot(time, deltadata1, lw = 0.5, c = 'orange', label = legp1)
-ax.legend()
-plt.ylabel("Changes of the blood pressure, mmHg")
-plt.xlabel("Time, seconds")
-plt.title("Pulse after fitness", fontsize = 25)
-ax.set_xlim(17, 30)
-ax.set_ylim(-3, 5)
-ax.grid(b=True, which='major', color='grey', linestyle='-')
-ax.grid(b=True, which='minor', color='grey', linestyle='--')
-plt.minorticks_on()
-
-figp2.savefig("fitness-pulse.png") 
-
-## CALM
-
-dataRawCalm = np.loadtxt("blood/data/rest.txt", dtype= int)
-dataCalm = dataRawCalm/k - y0/k
-time = np.linspace(0, 60, len(dataCalm))
-leg2 = str("Pressure = {}/{}".format(137,96))
-
-fig3, ax = plt.subplots(figsize = (16, 10), dpi = 400)
-ax.plot(time, dataCalm, lw = 1, c = 'brown', marker = 'o', markevery = [10.8*tstep, 37.5*tstep], label = leg2,
-markersize = 15, markerfacecolor = 'red', markeredgecolor = 'red')
-ax.legend()
-plt.ylabel("Pressure, mmHg")
-plt.xlabel("Time, seconds")
-plt.title("Blood pressure before fitness", fontsize = 25)
-ax.text(0.18, 0.68, 'Systole', fontsize = 20, transform=ax.transAxes)
-ax.text(0.79, 0.14, 'Diastole', fontsize = 20, transform=ax.transAxes)
-ax.grid(b=True, which='major', color='grey', linestyle='-')
-ax.grid(b=True, which='minor', color='grey', linestyle='--')
-plt.minorticks_on()
-
-fig3.savefig("rest-pressure.png")
-
-##CALM PULSE
-
-cplf = np.polyfit(time, dataCalm, 2)
-cyfit = np.polyval(cplf, time)
-deltadata2 = cyfit - dataCalm
-legp2 = str("The pulse is {:.0f} ".format(60*(12)/(28-18)))
-
-figp3, ax = plt.subplots(figsize = (16, 10), dpi = 400)
-ax.plot(time, deltadata2, lw = 0.5, c = 'orange', label = legp2)
-ax.legend()
-plt.ylabel("Changes of the blood pressure, mmHg")
-plt.xlabel("Time, seconds")
-plt.title("Pulse before fitness", fontsize = 25)
-ax.set_xlim(17, 30)
-ax.set_ylim(-2, 1.5)
-ax.grid(b=True, which='major', color='grey', linestyle='-')
-ax.grid(b=True, which='minor', color='grey', linestyle='--')
-plt.minorticks_on()
-
-figp3.savefig("rest-pulse.png") '''
+fig2.savefig("2.png")
